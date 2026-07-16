7446f50def03fc819e4546f833a977672964ae8c
Pooja Kanwar <poojakanwar6017@gmail.com>
2026-07-16 19:07:47 +0530

    Added deployment configuration for Render

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn Ruhava.wsgi:application
--- Ruhava/Ruhava/settings.py
+++ Ruhava/Ruhava/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--ysm@ting9hv%s9+1#$aibg@hfxs!v=(xp4enw5cz=_sxjhtuj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
