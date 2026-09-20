12508314abdd1a5e2fe3719802b32eac83cbb97a
Pooja Kanwar <poojakanwar6017@gmail.com>
2026-09-20 16:58:38 +0530

    Fix WSGI settings module

--- Ruhava/Ruhava/wsgi.py
+++ Ruhava/Ruhava/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ruhava.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ruhava.Ruhava.settings')
 
 application = get_wsgi_application()
